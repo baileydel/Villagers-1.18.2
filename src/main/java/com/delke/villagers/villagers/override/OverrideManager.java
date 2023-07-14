8ee47f0fbccd8d17c516e7ea74e62ed7914a1472
Bailey <delkerbailey@gmail.com>
2023-07-14 12:30:02 -0700

    Deleted

--- src/main/java/com/delke/villagers/villagers/override/OverrideManager.java
+++
@@ -1,9 +0,0 @@
-package com.delke.villagers.villagers.override;
-
-/**
- * @author Bailey Delker
- * @created 07/05/2023 - 8:32 PM
- * @project Villagers-1.18.2
- */
-public class OverrideManager {
-}
