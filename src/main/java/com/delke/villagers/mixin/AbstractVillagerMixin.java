b4f6e9c422221e8caad94da0caaf5c8ecbde27a8
Bailey <delkerbailey@gmail.com>
2023-07-29 20:34:26 -0700

    Updated trading mechanics

--- src/main/java/com/delke/villagers/mixin/AbstractVillagerMixin.java
+++ src/main/java/com/delke/villagers/mixin/AbstractVillagerMixin.java
@@ -1,11 +1,20 @@
 package com.delke.villagers.mixin;
 
+import com.delke.villagers.ExampleMod;
+import com.delke.villagers.villagers.VillagerUtil;
+import com.delke.villagers.villagers.profession.AbstractProfession;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerListener;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.npc.VillagerData;
+import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +24,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
+
 /**
  * @author Bailey Delker
  * @created 06/20/2023 - 5:59 PM
@@ -22,10 +33,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 
 @Mixin(AbstractVillager.class)
-public class AbstractVillagerMixin implements ContainerListener {
+public abstract class AbstractVillagerMixin implements ContainerListener {
 
     @Shadow
     private SimpleContainer inventory;
+
+    @Shadow @Nullable protected MerchantOffers offers;
 
     @Inject(
             method = "<init>",
@@ -36,6 +49,19 @@ public class AbstractVillagerMixin implements ContainerListener {
         inventory.addListener(this);
     }
 
+    @Inject(
+            method = "notifyTrade",
+            at = @At("TAIL")
+    )
+    private void notifyTrade(MerchantOffer offer, CallbackInfo ci) {
+        AbstractVillager villager = (AbstractVillager)(Object)this;
+        SimpleContainer inv = villager.getInventory();
+        ItemStack offerStack = offer.getResult();
+
+        if (VillagerUtil.hasEnoughOf(villager, offerStack)) {
+            inv.removeItemType(offerStack.getItem(), offerStack.getCount());
+        }
+    }
 
     @Inject(
             method = "canBeLeashed",
@@ -47,5 +73,29 @@ public class AbstractVillagerMixin implements ContainerListener {
     }
 
     @Override
-    public void containerChanged(@NotNull Container container) {}
+    public void containerChanged(@NotNull Container container) {
+        Villager villager = ((Villager)(Object)this);
+        VillagerData data = villager.getVillagerData();
+        VillagerProfession profession = data.getProfession();
+
+        MerchantOffers newOffers = new MerchantOffers();
+
+        // Add new trades when villagers inventory changes.
+        if (profession instanceof AbstractProfession abstractProfession) {
+            SimpleContainer inventory = villager.getInventory();
+            offers = newOffers;
+
+            for (ItemStack stack : abstractProfession.getProducibleItems()) {
+                for (int i = 0; i < inventory.getContainerSize(); i++) {
+                    ItemStack item = inventory.getItem(i);
+
+                    int count = item.getCount() / 6;
+
+                    if (stack.is(item.getItem()) && count >= 1) {
+                        offers.add(new ExampleMod.ItemsForEmeralds(item.getItem(), 1, 6, count, 1).getOffer(villager, villager.getRandom()));
+                    }
+                }
+            }
+        }
+    }
 }
