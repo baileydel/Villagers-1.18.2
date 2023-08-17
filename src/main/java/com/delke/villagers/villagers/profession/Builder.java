958f602cb7d8ee75a5ca0e763da960f3b5a016c6
Kent Stark <kkstark855@gmail.com>
2023-08-17 00:25:53 -0700

    Removed requested items

--- src/main/java/com/delke/villagers/villagers/profession/Builder.java
+++ src/main/java/com/delke/villagers/villagers/profession/Builder.java
@@ -7,11 +7,12 @@ import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.ArrayList;
+
 public class Builder extends AbstractProfession {
     public Builder() {
         super("builder",
                 VillagerManager.BUILDER_POI.get(),
-                ImmutableSet.of(),
                 ImmutableSet.of(),
                 SoundEvents.VILLAGER_WORK_FARMER
         );
